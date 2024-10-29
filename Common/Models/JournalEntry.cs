b2497fe01b3aa2943fbe5f5414be866e879957f9
Stephen Kauk <sdkauk@gmail.com>
2024-10-28 23:35:14 -0500

    Fixed some issues with the project references, added the nuget package MongoDb.Driver, and created JournalEntry model.

---
+++ Common/Models/JournalEntry.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Inkblot.Common.Models
+{
+    public class JournalEntry
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedUtc { get; set; }
+        public DateTime UpdateUtc { get; set; }
+        public string Content { get; set; }
+    }
+}
