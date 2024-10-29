4ab91fd70c0972ed1808d390cb7d86c709dec025
Stephen Kauk <sdkauk@gmail.com>
2024-10-29 00:43:44 -0500

    Created DbConnection class and IDbConnection interface

---
+++ DataAccess/DbConnection.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using Inkblot.Common.Models;
+using Inkblot.DataAccess;
+using Microsoft.Extensions.Configuration;
+
+namespace PaperTrade.DataAccess
+{
+    public class DbConnection : IDbConnection
+    {
+        private readonly IConfiguration configuration;
+        private readonly IMongoDatabase db;
+        private string connectionId = "MongoDB";
+        public string DbName { get; private set; }
+        public string JournalEntryCollectionName { get; private set; } = "journalentries";
+
+        public MongoClient Client { get; private set; }
+        public IMongoCollection<JournalEntry> JournalEntryCollection { get; private set; }
+
+        public DbConnection(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            Client = new MongoClient(configuration.GetConnectionString(connectionId));
+            DbName = configuration["DatabaseName"];
+            db = Client.GetDatabase(DbName);
+
+            JournalEntryCollection = db.GetCollection<JournalEntry>(JournalEntryCollectionName);
+        }
+
+    }
+}
---
+++ DataAccess/IDbConnection.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+using Inkblot.Common.Models;
+
+namespace Inkblot.DataAccess
+{
+    public interface IDbConnection
+    {
+        IMongoCollection<JournalEntry> JournalEntryCollection { get; }
+        string JournalEntryCollectionName { get; }
+        MongoClient Client { get; }
+    }
+}
